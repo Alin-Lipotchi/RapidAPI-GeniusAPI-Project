7c7b70b39d5b505ad38396feddc56815ed33b77b
Alin <alinlipotchi@yahoo.com>
2022-01-24 14:54:11 +0200

    adding the search system and card display

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ const searchForm = document.querySelector("form.search");
 const songsDiv = document.querySelector("div.songs");
 
 const listSongs = (e) => {
-	// songsDiv.innerHTML += ``;
 	console.log(e);
+	songsDiv.innerHTML += `	<div class="song-card">
+            <img src="${e.result.song_art_image_thumbnail_url}" alt="" class="album-cover">
+            <h2 class="artist-name">Artist: ${e.result.artist_names}</h2>
+            <h3 class="song-name">Song: ${e.result.title}</h3>
+            <a href="${e.result.url}" target="_blank" class="lyrics">Click here for lyrics!</a>
+        </div>`;
 }
 
 const search = async (e) => {
@@ -20,6 +25,7 @@ const search = async (e) => {
 	if(response.status === 200) {
 		const resultResponse = await response.json();
 		if(resultResponse.meta.status === 200) {
+			songsDiv.innerHTML = "";
 			resultResponse.response.hits.forEach(listSongs);
 		} else {
 			console.error("Something went wrong");
